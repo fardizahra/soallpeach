b9521779a1d0cc2568042ec08958c074d9c911e7
unknown <Administrator@WSN-021.systemgroup.net>
2020-04-22 16:48:17 +0430

    prime

--- prime/prime.py
+++ prime/prime.py
@@ -9,7 +9,7 @@ def IsPrime(n):
         return False
 
     i = 5
-    while i <= (math.sqrt(n)+1):
+    while i * i <= n :
         if n % i == 0 or n % (i + 2) == 0:
             return False
         i = i + 6
