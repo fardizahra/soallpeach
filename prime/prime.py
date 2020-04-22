6e8f278200fcdf7359ba0a794f472a21cc308235
unknown <Administrator@WSN-021.systemgroup.net>
2020-04-22 16:36:07 +0430

    prime

--- prime/prime.py
+++ prime/prime.py
@@ -1,16 +1,15 @@
 import math, sys
-#import time
-#start_time = time.time()
 file_name = sys.argv[1]
 
 def IsPrime(n):
     if n>= 1 and n <= 3:
         return True
+    
     if n % 2 == 0 or n % 3 == 0:
         return False
 
     i = 5
-    while i <= math.sqrt(n)+1:
+    while i <= (math.sqrt(n)+1):
         if n % i == 0 or n % (i + 2) == 0:
             return False
         i = i + 6
@@ -19,6 +18,4 @@ def IsPrime(n):
 with open(file_name) as inputs:
     for input_data in inputs: 
         print(1 if IsPrime(int(input_data)) else 0)
-
-#duration = time.time() - start_time
 
